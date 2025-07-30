265c173dd296fba0116e59271d0c029ff7eb38ca
Kirk Kudoto <135407806+Kirk-kud@users.noreply.github.com>
2025-07-30 10:33:37 +0000

    Making run time configuration changes

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import uvicorn
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+HOST = os.getenv("HOST")
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("app:app", host=HOST, port=8080, reload=True)
 # localhost: 127.0.0.1
 # online deployment: 0.0.0.0"
